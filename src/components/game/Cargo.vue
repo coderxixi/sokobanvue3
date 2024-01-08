329a77490d0df0645dcf10b8e4312f3b1bf270fa
liuss7 <976344695@qq.com>
2024-01-08 09:06:10 +0800

    渲染箱子

---
+++ src/components/game/Cargo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class=" absolute" :style="position">
+    <img :src="CargoImg" alt="">
+  </div>
+</template>
+
+<script setup>
+import CargoImg from "../../assets/cargo.png";
+import { computed } from "vue"
+const cargo = {
+  x: 2,
+  y: 2
+}
+function usePostion(){
+  const STEP = 32;
+  const position = computed(() => {
+    return {
+      left: cargo.x * STEP + 'px',
+      top: cargo.y * STEP + 'px'
+    }
+  })
+  return {
+    position
+  }
+}
+
+const { position } = usePostion()
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -4,12 +4,15 @@
     <Map></Map>
     <!--玩家 -->
     <Player></Player>
+    <!-- 箱子 -->
+    <Cargo></Cargo>
   </div>
 </template>
 
 <script setup>
 import Map from "./Map.vue";
 import Player from "./Player.vue"
+import Cargo from './Cargo.vue'
 </script>
 
 <style lang="scss" scoped></style>
